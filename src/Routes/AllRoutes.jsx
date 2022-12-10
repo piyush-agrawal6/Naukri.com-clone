504b655700e23340317e7fcfb3bf243eacbd45a1
piyush-agrawal6 <agrawaljoy1@gmail.com>
2022-12-10 14:57:23 +0530

    structure  updated

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Admin from "../Pages/Admin/Admin";
+import Company from "../Pages/Company/Company";
+import Home from "../Pages/Home/Home";
+import Jobs from "../Pages/Jobs/Jobs";
+import Login from "../Pages/Login/Login";
+import Profile from "../Pages/Profile/Profile";
+import Register from "../Pages/Register/Register";
 const AllRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>Routes</h1>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/jobs" element={<Jobs />}></Route>
+        <Route path="/company" element={<Company />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/admin" element={<Admin />}></Route>
       </Routes>
     </>
   );
